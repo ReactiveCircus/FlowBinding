afe4fec796117cd4af512c76dacc663d24ef2d53
Yang Chen <ychescale9@gmail.com>
2019-08-17 23:57:56 +1000

    Use static import for LIBRARY_GROUP.

--- flowbinding-common/src/main/java/reactivecircus/flowbinding/common/StartWithCurrentValue.kt
+++ flowbinding-common/src/main/java/reactivecircus/flowbinding/common/StartWithCurrentValue.kt
@@ -1,6 +1,7 @@
 package reactivecircus.flowbinding.common
 
 import androidx.annotation.RestrictTo
+import androidx.annotation.RestrictTo.Scope.LIBRARY_GROUP
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.onStart
@@ -8,7 +9,7 @@ import kotlinx.coroutines.flow.onStart
 /**
  * Create a flow that emits the value returned by [block] immediately if [emitImmediately] is true.
  */
-@RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
+@RestrictTo(LIBRARY_GROUP)
 @UseExperimental(ExperimentalCoroutinesApi::class)
 fun <T> Flow<T>.startWithCurrentValue(emitImmediately: Boolean, block: () -> T): Flow<T> {
     return if (emitImmediately) onStart { emit(block()) } else this
