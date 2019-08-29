80da9b0fdd7c4bfd756eeb7e445ee4256ae5020c
Yang Chen <ychescale9@gmail.com>
2019-08-29 21:02:45 +1000

    Add bindings for TabLayout.

--- flowbinding-material/fixtures/src/main/java/reactivecircus/flowbinding/material/fixtures/MaterialFragment2.kt
+++ flowbinding-material/fixtures/src/main/java/reactivecircus/flowbinding/material/fixtures/MaterialFragment2.kt
@@ -1,12 +1,5 @@
 package reactivecircus.flowbinding.material.fixtures
 
-import android.os.Bundle
-import android.view.View
 import androidx.fragment.app.Fragment
 
-class MaterialFragment2 : Fragment(R.layout.fragment_material_2) {
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-    }
-}
+class MaterialFragment2 : Fragment(R.layout.fragment_material_2)
