6da8ff2fa202a31cc603499eed62d59b33e4263d
Yang Chen <ychescale9@gmail.com>
2019-09-12 01:33:55 +1000

    Introduce flowbinding-android for platform bindings. Add view bindings for clicked, long clicked and focus changed events.

---
+++ flowbinding-android/src/main/java/reactivecircus/flowbinding/android/view/ViewClickedFlow.kt
@@ -0,0 +1,38 @@
+package reactivecircus.flowbinding.android.view
+
+import android.view.View
+import androidx.annotation.CheckResult
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.conflate
+import reactivecircus.flowbinding.common.checkMainThread
+import reactivecircus.flowbinding.common.safeOffer
+
+/**
+ * Create a [Flow] of click events on the [View] instance.
+ *
+ * Note: Created flow keeps a strong reference to the [View] instance
+ * until the coroutine that launched the flow collector is cancelled.
+ *
+ * Example of usage:
+ *
+ * ```
+ * view.clicks()
+ *     .onEach {
+ *          // handle view clicked
+ *     }
+ *     .launchIn(uiScope)
+ * ```
+ */
+@CheckResult
+@UseExperimental(ExperimentalCoroutinesApi::class)
+fun View.clicks(): Flow<Unit> = callbackFlow {
+    checkMainThread()
+    val listener = View.OnClickListener {
+        safeOffer(Unit)
+    }
+    setOnClickListener(listener)
+    awaitClose { setOnClickListener(null) }
+}.conflate()
---
+++ flowbinding-android/src/main/java/reactivecircus/flowbinding/android/view/ViewFocusChangedFlow.kt
@@ -0,0 +1,44 @@
+package reactivecircus.flowbinding.android.view
+
+import android.view.View
+import androidx.annotation.CheckResult
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.conflate
+import reactivecircus.flowbinding.common.checkMainThread
+import reactivecircus.flowbinding.common.safeOffer
+import reactivecircus.flowbinding.common.startWithCurrentValue
+
+/**
+ * Create a [Flow] of focus changed events on the [View] instance,
+ * where the value emitted indicates whether the [View] has focus.
+ *
+ * Note: Created flow keeps a strong reference to the [View] instance
+ * until the coroutine that launched the flow collector is cancelled.
+ *
+ * @param emitImmediately whether to emit the current value (if any) immediately on flow collection.
+ *
+ * Example of usage:
+ *
+ * ```
+ * view.focusChanges()
+ *     .onEach {
+ *          // handle view focus changed
+ *     }
+ *     .launchIn(uiScope)
+ * ```
+ */
+@CheckResult
+@UseExperimental(ExperimentalCoroutinesApi::class)
+fun View.focusChanges(emitImmediately: Boolean = false): Flow<Boolean> = callbackFlow {
+    checkMainThread()
+    val listener = View.OnFocusChangeListener { _, hasFocus ->
+        safeOffer(hasFocus)
+    }
+    onFocusChangeListener = listener
+    awaitClose { onFocusChangeListener = null }
+}
+    .startWithCurrentValue(emitImmediately) { hasFocus() }
+    .conflate()
