a21b6282d90d9cacf88119a84fa7b196428c74ce
Yang Chen <ychescale9@gmail.com>
2019-10-21 20:11:16 +1100

    Add bindings for popup menu.

---
+++ flowbinding-android/src/main/java/reactivecircus/flowbinding/android/widget/PopupMenuDismissFlow.kt
@@ -0,0 +1,38 @@
+package reactivecircus.flowbinding.android.widget
+
+import android.widget.PopupMenu
+import androidx.annotation.CheckResult
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.conflate
+import reactivecircus.flowbinding.common.checkMainThread
+import reactivecircus.flowbinding.common.safeOffer
+
+/**
+ * Create a [Flow] of dismiss events on the [PopupMenu] instance.
+ *
+ * Note: Created flow keeps a strong reference to the [PopupMenu] instance
+ * until the coroutine that launched the flow collector is cancelled.
+ *
+ * Example of usage:
+ *
+ * ```
+ * popupMenu.dismisses()
+ *     .onEach { menuItem ->
+ *          // handle popup menu dismiss event
+ *     }
+ *     .launchIn(uiScope)
+ * ```
+ */
+@CheckResult
+@UseExperimental(ExperimentalCoroutinesApi::class)
+fun PopupMenu.dismisses(): Flow<Unit> = callbackFlow {
+    checkMainThread()
+    val listener = PopupMenu.OnDismissListener {
+        safeOffer(Unit)
+    }
+    setOnDismissListener(listener)
+    awaitClose { setOnDismissListener(null) }
+}.conflate()
