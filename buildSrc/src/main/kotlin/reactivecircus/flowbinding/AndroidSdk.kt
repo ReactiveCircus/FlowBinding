2c6b192a84eb90beb4f19b8b349bac3530d9294a
Yang Chen <ychescale9@gmail.com>
2020-01-31 11:14:27 +1100

    Build tools 29.0.3.

--- buildSrc/src/main/kotlin/reactivecircus/flowbinding/AndroidSdk.kt
+++ buildSrc/src/main/kotlin/reactivecircus/flowbinding/AndroidSdk.kt
@@ -6,5 +6,5 @@ object androidSdk {
     const val minSdk = 21
     const val targetSdk = 29
     const val compileSdk = 29
-    const val buildTools = "29.0.2"
+    const val buildTools = "29.0.3"
 }
