71e9c189b7218b460a4058770db4b3879c2abdd8
Yang Chen <ychescale9@gmail.com>
2020-04-02 23:21:37 +1100

    Fix instrumented test.

--- flowbinding-swiperefreshlayout/fixtures/src/main/java/reactivecircus/flowbinding/swiperefreshlayout/fixtures/SwipeRefreshLayoutFragment.kt
+++ flowbinding-swiperefreshlayout/fixtures/src/main/java/reactivecircus/flowbinding/swiperefreshlayout/fixtures/SwipeRefreshLayoutFragment.kt
@@ -1,21 +1,5 @@
 package reactivecircus.flowbinding.swiperefreshlayout.fixtures
 
-import android.os.Bundle
-import android.view.View
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
-import reactivecircus.flowbinding.swiperefreshlayout.fixtures.databinding.FragmentSwipeRefreshLayoutBinding
 
-class SwipeRefreshLayoutFragment : Fragment(R.layout.fragment_swipe_refresh_layout) {
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        val binding = FragmentSwipeRefreshLayoutBinding.bind(view)
-
-        @Suppress("MagicNumber")
-        binding.recyclerView.apply {
-            layoutManager = LinearLayoutManager(requireContext())
-            adapter = SimpleAdapter(20)
-        }
-    }
-}
+class SwipeRefreshLayoutFragment : Fragment(R.layout.fragment_swipe_refresh_layout)
