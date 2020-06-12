a4248da33b1d3aa1cc395a5a6cdaa31f0dc482e9
Yang <ychescale9@gmail.com>
2020-06-12 23:25:49 +1000

    Update build tools to 30.0.0, update compileSdk and targetSdk to API 30.

--- buildSrc/src/main/kotlin/reactivecircus/flowbinding/AndroidSdk.kt
+++ buildSrc/src/main/kotlin/reactivecircus/flowbinding/AndroidSdk.kt
@@ -5,7 +5,7 @@ package reactivecircus.flowbinding
 object androidSdk {
     const val minSdk = 14
     const val testMinSdk = 21
-    const val targetSdk = 29
-    const val compileSdk = 29
-    const val buildTools = "29.0.3"
+    const val targetSdk = 30
+    const val compileSdk = 30
+    const val buildTools = "30.0.0"
 }
