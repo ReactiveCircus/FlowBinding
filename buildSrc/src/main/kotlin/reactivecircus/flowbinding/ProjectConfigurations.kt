d04f560db74a537714e137ab5b0923dec936ba72
Yang <ychescale9@gmail.com>
2020-06-19 02:46:40 +1000

    Remove unused import.

--- buildSrc/src/main/kotlin/reactivecircus/flowbinding/ProjectConfigurations.kt
+++ buildSrc/src/main/kotlin/reactivecircus/flowbinding/ProjectConfigurations.kt
@@ -3,7 +3,6 @@ package reactivecircus.flowbinding
 import com.android.build.gradle.LibraryExtension
 import com.android.build.gradle.TestedExtension
 import org.gradle.StartParameter
-import org.gradle.api.Action
 import org.gradle.api.JavaVersion
 import org.gradle.api.Project
 import org.gradle.api.tasks.Delete
