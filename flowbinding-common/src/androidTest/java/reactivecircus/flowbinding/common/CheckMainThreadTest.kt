9c5977dc6ef80f611993d588a31da725a380173e
Yang <reactivecircus@gmail.com>
2021-12-25 02:42:38 +1100

    Fix test.

--- flowbinding-common/src/androidTest/java/reactivecircus/flowbinding/common/CheckMainThreadTest.kt
+++ flowbinding-common/src/androidTest/java/reactivecircus/flowbinding/common/CheckMainThreadTest.kt
@@ -2,32 +2,24 @@ package reactivecircus.flowbinding.common
 
 import androidx.test.annotation.UiThreadTest
 import androidx.test.filters.MediumTest
-import com.google.common.truth.Truth.assertThat
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.single
-import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertThrows
 import org.junit.Test
 
 @MediumTest
 @ExperimentalCoroutinesApi
 class CheckMainThreadTest {
 
-    private val mainThreadFlow = flow {
-        checkMainThread()
-        emit(true)
-    }
-
     @Test
     @UiThreadTest
-    fun checkMainThread_onMainThread() = runTest {
-        assertThat(mainThreadFlow.single())
-            .isTrue()
+    fun checkMainThread_onMainThread() {
+        checkMainThread()
     }
 
     @Test
-    fun checkMainThread_notMainThread() = runTest {
-        assertThrows<IllegalStateException> { mainThreadFlow.collect() }
+    fun checkMainThread_notMainThread() {
+        assertThrows(IllegalStateException::class.java) {
+            checkMainThread()
+        }
     }
 }
