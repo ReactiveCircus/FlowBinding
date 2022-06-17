d1557d22ce40bbac09e92d9553a97ac7a8bc3a3c
Yang <reactivecircus@gmail.com>
2022-06-17 23:45:41 +1000

    Target API 33.

--- buildSrc/src/main/kotlin/reactivecircus/flowbinding/AndroidSdk.kt
+++ buildSrc/src/main/kotlin/reactivecircus/flowbinding/AndroidSdk.kt
@@ -5,7 +5,7 @@ package reactivecircus.flowbinding
 object androidSdk {
     const val minSdk = 14
     const val testMinSdk = 21
-    const val targetSdk = 32
-    const val compileSdk = 32
-    const val buildTools = "32.0.0"
+    const val targetSdk = 33
+    const val compileSdk = 33
+    const val buildTools = "33.0.0"
 }
